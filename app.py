abbd1a3f80b48a99e3e28f9bd877c7e0b8d89cf4
Andrius Bai <abaikstys@gmail.com>
2021-09-05 19:08:31 +0300

    first system test

---
+++ app.py
@@ -0,0 +1,16 @@
+blogs = dict() # blog_name: Blog objects
+
+
+def menu():
+    # Show the user the available blogs
+    # Let the user make a choice
+    # Do something with that choice
+    # Eventually exit
+
+    print_blogs()
+
+
+def print_blogs():
+    # Print available blogs
+    for key, blog in blogs.items():
+        print(f"- {blog}")
