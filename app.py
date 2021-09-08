13d8918a0134ec5b2079e2dec3efc3c4bc7cb834
Andrius Bai <abaikstys@gmail.com>
2021-09-08 16:04:42 +0300

    64_lesson_patching

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from post import Post
+from blog import Blog
+
+
 MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, or "q" to quit: '
 
 blogs = dict() # blog_name: Blog objects
@@ -11,9 +15,47 @@ def menu():
 
     print_blogs()
     selection = input(MENU_PROMPT)
+    while selection != "q":
+        if selection == 'l':
+            print_blogs()
+        elif selection == 'c':
+            ask_create_blog()
+        elif selection == 'r':
+            ask_read_blog()
+        elif selection == 'p':
+            ask_create_post()
+        selection = input(MENU_PROMPT)
 
 
 def print_blogs():
     # Print available blogs
     for key, blog in blogs.items():
         print(f"- {blog}")
+
+
+def ask_create_blog():
+    title = input('Enter blog title: ')
+    author = input('Enter blog author name: ')
+    blogs[title] = Blog(title, author)
+
+
+def ask_read_blog():
+    title = input('Enter the blog title you want to read: ')
+    print_posts(blogs[title])
+
+
+def print_posts(blog):
+    for post in blog.posts:
+        print_post(post)
+
+
+def print_post(post):
+    print(f"Title ---{post.title}---"
+          f""
+          f"Content {post.content}"
+          f"")
+
+
+def ask_create_post():
+    pass
+
